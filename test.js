ef318df633ebe2ae24ee0f112ac5af911411cba7
Ikhizamah Jewel <jewelhckr@gmail.com>
2024-05-08 09:05:03 +0100

    Created queue and stack
    Uploaded maually

---
+++ test.js
@@ -0,0 +1,90 @@
+// DSA Test
+
+// Implement a stack using an array - length, push, pop, peekLast, peekFirst, isEmpty, print
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  length() {
+    return this.items.length;
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop = ()=> {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  peekLast() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  peekFirst() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.print();
+console.log(stack.pop());
+console.log(stack.peekLast());
+console.log(stack.peekFirst());
+console.log(stack.isEmpty());
+console.log(stack.length());
+
+// Implement a queue using an object - isFull, isEmpty, Enqueue, dequeue, size, print
+
+class Queue {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  isFull() {
+    return Object.keys(this.items).length === this.rear;
+  }
+
+  isEmpty() {
+    return this.front === this.rear;
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isFull)
+console.log(queue.isEmpty());
+console.log(queue.length());
+console.log(queue.size());
+queue.print();
